4b6d8ef0797efea8b1a30ac10d2d22e7627219d1
Brandon <bgonzalez526@students.mchenry.edu>
2018-09-11 11:12:50 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        String[] ar = {"Sugar", "Powder Creamer", "Caramel", "Chocolate", "Milk"};
+
+        for (int i = 0; i < ar.length; i++){
+            System.out.println(ar[i]);
+        }
+    }
+}
